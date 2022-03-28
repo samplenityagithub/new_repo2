6ed81d9496b35aa6c8fdb199d567cacb0c91f8aa
Nitya Singh <101619574+samplenityagithub@users.noreply.github.com>
2022-03-28 10:58:48 +0530

    Add files via upload

---
+++ Q8.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Q8 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);  
+	 
+	for (int x=2;x<=8;x++) {
+	 for(int i=1; i <= 10; i++)  
+ 	{  
+	    System.out.println(x+" * "+i+" = "+x*i);  
+    }  
+		
+	}
+ }
+}
